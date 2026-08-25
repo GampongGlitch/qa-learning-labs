265588b915a58af299fd00fafc425bd6ee60d2dd
GampongGlitch <272003664+GampongGlitch@users.noreply.github.com>
2026-08-25 21:46:30 +0700

    update(php): update lab01 product information

--- 01-php-fundamentals/chapter-01-variables-data-types/lab01_product_information.php
+++ 01-php-fundamentals/chapter-01-variables-data-types/lab01_product_information.php
@@ -19,15 +19,15 @@ echo "-----------------------------\n";
 
 // Exercise 2: Product Information
 
-$productName2 = "Smartphone";
-$price2 = 20000.00;
-$quantity2 = 10;
-$isAvailable2 = false;
+$productName2 = "Smartphone"; // ชื่อสินค้า
+$price2 = 20000.00; // ราคาต่อหน่วย
+$quantity2 = 10; // จํานวน
+$isAvailable2 = false; // สถานะ
 
-$total2 = $price2 * $quantity2;
+$total2 = $price2 * $quantity2; // ยอดรวม = ราคาต่อหน่วย * จํานวน
 
-echo "Product Name: " . $productName2 . "\n";
-echo "Price: " . $price2 . " THB\n";
-echo "Quantity: " . $quantity2 . "\n";
-echo "Status: " . ($isAvailable2 ? "In Stock" : "Out of Stock") . "\n";
-echo "Total: " . number_format($total2, 2) . " THB\n";
+echo "Product Name: " . $productName2 . "\n"; // แสดงชื่อสินค้า
+echo "Price: " . $price2 . " THB\n"; // แสดงราคา
+echo "Quantity: " . $quantity2 . "\n"; // แสดงจํานวน
+echo "Status: " . ($isAvailable2 ? "In Stock" : "Out of Stock") . "\n"; // แสดงสถานะ (ถ้า $isAvailable2 เป็น true จะแสดง "In Stock" ถ้าเป็น false จะแสดง "Out of Stock")
+echo "Total: " . number_format($total2, 2) . " THB\n"; // แสดงยอดรวม (รูปแบบตัวเลขมีทศนิยม 2 ตําแหน่ง)
